32a196ac87e0d20148fc1d086e7422d3dce01a1d
advaitsp <65618239+advaitsp@users.noreply.github.com>
2020-05-21 13:18:07 +0530

    Add files via upload

---
+++ String4.py
@@ -0,0 +1,11 @@
+# Python Program to Update 
+# entire String 
+
+String1 = "Hello, This is Advait"
+print("Initial String: ") 
+print(String1) 
+
+# Updating a String 
+String1 = "Welcome to Programming in Python"
+print("\nUpdated String: ") 
+print(String1) 
