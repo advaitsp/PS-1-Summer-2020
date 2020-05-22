1b7761e3b28e5f6dc19613c1ea70dbf142ef619b
advaitsp <65618239+advaitsp@users.noreply.github.com>
2020-05-22 13:45:47 +0530

    Add files via upload

---
+++ Python/Sets/Set1.py
@@ -0,0 +1,27 @@
+# Python program to demonstrate 
+# Creation of Set in Python 
+
+# Creating a Set 
+set1 = set() 
+print("Intial blank Set: ") 
+print(set1) 
+
+# Creating a Set with 
+# the use of a String 
+set1 = set("PythonForEveryone") 
+print("\nSet with the use of String: ") 
+print(set1) 
+
+# Creating a Set with 
+# the use of Constructor 
+# (Using object to Store String) 
+String = 'PythonForEveryone'
+set1 = set(String) 
+print("\nSet with the use of an Object: " ) 
+print(set1) 
+
+# Creating a Set with 
+# the use of a List 
+set1 = set(["Python", "For", "Everyone"]) 
+print("\nSet with the use of List: ") 
+print(set1) 
