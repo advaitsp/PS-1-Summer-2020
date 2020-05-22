c8fc3a9dc6f0a36c2c92354bb0e6510e82f930dd
advaitsp <65618239+advaitsp@users.noreply.github.com>
2020-05-22 13:43:46 +0530

    Add files via upload

---
+++ Python/Strings/String1.py
@@ -0,0 +1,28 @@
+# Python Program for 
+# Creation of String 
+
+# Creating a String 
+# with single Quotes 
+String1 = 'Welcome to Python'
+print("String with the use of Single Quotes: ") 
+print(String1) 
+
+# Creating a String 
+# with double Quotes 
+String1 = "Python is the most trending language"
+print("\nString with the use of Double Quotes: ") 
+print(String1) 
+
+# Creating a String 
+# with triple Quotes 
+String1 = '''I like programming in "Python"'''
+print("\nString with the use of Triple Quotes: ") 
+print(String1) 
+
+# Creating String with triple 
+# Quotes allows multiple lines 
+String1 = '''Python
+			For 
+			Everyone'''
+print("\nCreating a multiline String: ") 
+print(String1) 
