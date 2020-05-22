5b2f6d1f94907a7094e0b157af548031391cd05e
advaitsp <65618239+advaitsp@users.noreply.github.com>
2020-05-22 13:04:04 +0530

    Add files via upload

---
+++ Set4.py
@@ -0,0 +1,17 @@
+# Python program to demonstrate 
+# Accessing of elements in a set 
+
+# Creating a set 
+set1 = set(["Python", "For", "Everyone"]) 
+print("\nInitial set") 
+print(set1) 
+
+# Accessing element using 
+# for loop 
+print("\nElements of set: ") 
+for i in set1: 
+	print(i, end=" ") 
+
+# Checking the element 
+# using in keyword 
+print("Python" in set1) 
