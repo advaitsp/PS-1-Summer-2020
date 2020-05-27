7c08764e33e719270c5657f9ca39c0f5e2221b29
advaitsp <65618239+advaitsp@users.noreply.github.com>
2020-05-27 12:43:50 +0530

    Add files via upload

---
+++ Python/IO/Directory.py
@@ -0,0 +1,36 @@
+# Directories in python
+
+# Make a Directory
+
+# os.mkdir("newdir")
+
+import os
+
+# Create a directory "test"
+
+os.mkdir("test")
+
+
+# Change Directory
+
+# os.chdir("newdir")
+
+os.chdir("/home/newdir")
+
+
+
+# current Working directory
+
+# os.getcwd
+
+os.getcwd()
+
+
+
+
+
+# Remmove directory
+
+# os.rmdir('dirname')
+
+os.rmdir( "/tmp/test"  
