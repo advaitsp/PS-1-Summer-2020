3bb5b49c19957942413ef5c65a271aed8e528a87
advaitsp <65618239+advaitsp@users.noreply.github.com>
2020-05-27 12:41:56 +0530

    Add files via upload

---
+++ Python/Exception Handling/Exception_Basics.py
@@ -0,0 +1,41 @@
+# Raising an Exception
+
+# raise [Exception [, args [, traceback]]]
+
+# def functionName( level ):
+
+#   if level <1:
+
+#      raise Exception(level)
+
+       # The code below to this would not be executed
+
+       # if we raise the exception
+
+#   return level
+
+
+
+def functionName( level ):
+
+   if level <1:
+
+      raise Exception(level)
+
+      # The code below to this would not be executed
+
+      # if we raise the exception
+
+   return level
+
+
+
+try:
+
+   l = functionName(-10)
+
+   print ("level = ",l)
+
+except Exception as e:
+
+   print ("error in level argument",e.args[0])
