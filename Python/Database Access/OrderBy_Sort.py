b919cb48cf6d9ec0f9e61a5cb97790360dafd8f1
advaitsp <65618239+advaitsp@users.noreply.github.com>
2020-05-29 13:30:29 +0530

    Create OrderBy_Sort.py

---
+++ Python/Database Access/OrderBy_Sort.py
@@ -0,0 +1,44 @@
+# Sort the result
+# alphabetically by name
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="yourusername",
+  passwd="yourpassword",
+  database="mydatabase"
+)
+
+mycursor = mydb.cursor()
+
+sql = "SELECT * FROM customers ORDER BY name"
+
+mycursor.execute(sql)
+
+myresult = mycursor.fetchall()
+
+for x in myresult:
+  print(x)
+ 
+# Sort the result 
+# reverse alphabetically by name
+ 
+  import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="yourusername",
+  passwd="yourpassword",
+  database="mydatabase"
+)
+
+mycursor = mydb.cursor()
+
+sql = "SELECT * FROM customers ORDER BY name DESC"
+
+mycursor.execute(sql)
+
+myresult = mycursor.fetchall()
+
+for x in myresult:
+  print(x) 
