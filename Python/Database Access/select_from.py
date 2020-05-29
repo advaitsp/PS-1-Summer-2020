46d1351a2a5c2826a45a91a44ec11678c3226ff3
advaitsp <65618239+advaitsp@users.noreply.github.com>
2020-05-29 11:18:06 +0530

    Create select_from.py

---
+++ Python/Database Access/select_from.py
@@ -0,0 +1,20 @@
+# Select only the 
+# name and address columns
+
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="yourusername",
+  passwd="yourpassword",
+  database="mydatabase"
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("SELECT name, address FROM customers")
+
+myresult = mycursor.fetchall()
+
+for x in myresult:
+  print(x) 
