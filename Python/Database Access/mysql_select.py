afa60ca01978e2aa8983e6e8289a302ea07b56c7
advaitsp <65618239+advaitsp@users.noreply.github.com>
2020-05-29 11:17:21 +0530

    Create mysql_select.py

---
+++ Python/Database Access/mysql_select.py
@@ -0,0 +1,20 @@
+# Select From a Table
+
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="yourusername",
+  passwd="yourpassword",
+  database="mydatabase"
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("SELECT * FROM customers")
+
+myresult = mycursor.fetchall()
+
+for x in myresult:
+  print(x)
+  
