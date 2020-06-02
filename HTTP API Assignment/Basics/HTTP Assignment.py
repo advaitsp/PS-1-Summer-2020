0d5c3bd2aa5c4e12ab45565a2c8cc95adae2ab6c
advaitsp <65618239+advaitsp@users.noreply.github.com>
2020-06-02 16:58:23 +0530

    Create HTTP Assignment.py

---
+++ HTTP API Assignment/Basics/HTTP Assignment.py
@@ -0,0 +1,194 @@
+#!/usr/bin/env python
+
+from flask import (Flask, jsonify, request, abort, )
+
+import math
+
+app = Flask(__name__)
+
+
+
+
+@app.route('/add', methods=['POST'])
+
+def add_args():
+
+    if not request.json:
+
+        abort(400)
+
+    try:
+
+        arg1 = request.json['argument1']
+
+        arg2 = request.json['argument2']
+
+        answer = arg1 + arg2
+
+        return (jsonify({'answer':answer}), 200)
+
+    except KeyError:
+
+        abort(400)
+
+
+
+@app.route('/subtract', methods=['POST'])
+
+def subtract_args():
+
+    if not request.json:
+
+        abort(400)
+
+    try:
+
+        arg1 = request.json['argument1']
+
+        arg2 = request.json['argument2']
+
+        answer = arg1 - arg2
+
+        return (jsonify({'answer':answer}), 200)
+
+    except KeyError:
+
+        abort(400)
+
+
+
+@app.route('/multiply', methods=['POST'])
+
+def multiply_args():
+
+    if not request.json:
+
+        abort(400)
+
+    try:
+
+        arg1 = request.json['argument1']
+
+        arg2 = request.json['argument2']
+
+        answer = arg1 * arg2
+
+        return (jsonify({'answer':answer}), 200)
+
+    except KeyError:
+
+        abort(400)
+
+
+
+@app.route('/divide', methods=['POST'])
+
+def divide_args():
+
+    if not request.json:
+
+        abort(400)
+
+    try:
+
+        arg1 = request.json['argument1']
+
+        arg2 = request.json['argument2']
+
+        answer = arg1 / arg2
+
+        return (jsonify({'answer':answer}), 200)
+
+    except KeyError:
+
+        abort(400)
+
+    except ZeroDivisionError:
+
+        abort(400)
+
+
+
+@app.route('/mod', methods=['POST'])
+
+def mod_args():
+
+    if not request.json:
+
+        abort(400)
+
+    try:
+
+        arg1 = request.json['argument1']
+
+        arg2 = request.json['argument2']
+
+        answer = arg1 % arg2
+
+        return (jsonify({'answer':answer}), 200)
+
+    except KeyError:
+
+        abort(400)
+
+    except ZeroDivisionError:
+
+        abort(400)
+
+
+
+@app.route('/exp', methods=['POST'])
+
+def exponent_args():
+
+    if not request.json:
+
+        abort(400)
+
+    try:
+
+        arg1 = request.json['argument1']
+
+        arg2 = request.json['argument2']
+
+        answer = arg1 ** arg2
+
+        return (jsonify({'answer':answer}), 200)
+
+    except KeyError:
+
+        abort(400)
+
+
+
+@app.route('/average', methods=['POST'])
+
+def average_args():
+
+    if not request.json:
+
+        abort(400)
+
+    try:
+
+        arg1 = request.json['argument1']
+
+        arg2 = request.json['argument2']
+
+        answer = (float(arg1) + float(arg2)) / 2
+
+        return (jsonify({'answer':answer}), 200)
+
+    except KeyError:
+
+        abort(400)
+
+
+
+
+
+
+
+if __name__ == '__main__':
+
+    app.run(debug=True)
