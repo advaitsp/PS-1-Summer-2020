165fe65cfea6b3cd36cb63b90ef975ce49788505
advaitsp <65618239+advaitsp@users.noreply.github.com>
2020-06-03 12:44:29 +0530

    Create company.py

---
+++ Database Assignment/company.py
@@ -0,0 +1,14 @@
+class Company(object):
+
+    
+    def __init__(self,name,field,MD,type):
+
+        
+        self.name = name
+        
+        self.field = field
+        
+        self.MD = MD
+        
+        slef.type = type
+        
