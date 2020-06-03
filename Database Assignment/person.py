b0b80d153a9d8d8c6586560493fcf768a88894c2
advaitsp <65618239+advaitsp@users.noreply.github.com>
2020-06-03 12:45:29 +0530

    Create person.py

---
+++ Database Assignment/person.py
@@ -0,0 +1,12 @@
+class Person(object):
+
+    
+    def __init__(self,f_name,l_name,age,gender):
+        
+        self.f_name = f_name
+        
+        self.l_name = l_name
+        
+        self.age = age
+        
+        self.gender = gender
