dce53aeb5a3d506e3c2d326a73ad189148363cf9
advaitsp <65618239+advaitsp@users.noreply.github.com>
2020-06-05 15:01:52 +0530

    Update Calculator.py

--- HTTP API Assignment/API Calculator/Calculator.py
+++ HTTP API Assignment/API Calculator/Calculator.py
@@ -56,7 +56,62 @@ def divide_args():
         abort(400)
     except ZeroDivisionError:
         abort(400)
-        
+
+@app.route('/mod', methods=['POST'])
+def mod_args():
+    if not request.json:
+        abort(400)
+    try:
+        arg1 = request.json['argument1']
+        arg2 = request.json['argument2']
+        answer = arg1 % arg2
+        return (jsonify({'answer':answer}), 200)
+    except KeyError:
+        abort(400)
+    except ZeroDivisionError:
+        abort(400)
+
+@app.route('/exp', methods=['POST'])
+def exponent_args():
+    if not request.json:
+        abort(400)
+    try:
+        arg1 = request.json['argument1']
+        arg2 = request.json['argument2']
+        answer = arg1 ** arg2
+        return (jsonify({'answer':answer}), 200)
+    except KeyError:
+        abort(400)
+
+@app.route('/average', methods=['POST'])
+def average_args():
+    if not request.json:
+        abort(400)
+    try:
+        arg1 = request.json['argument1']
+        arg2 = request.json['argument2']
+        answer = (float(arg1) + float(arg2)) / 2
+        return (jsonify({'answer':answer}), 200)
+    except KeyError:
+        abort(400)
+
+@app.route('/log', methods=['POST'])
+def log_args():
+    if not request.json:
+        abort(400)
+    try:
+        arg1 = request.json['argument1']
+        arg2 = request.json['argument2']
+        answer = math.log(arg1, arg2)
+        return (jsonify({'answer':answer}), 200)
+    except KeyError:
+        abort(400)
+    except ZeroDivisionError:
+        # occurs when you try a log(a,1)
+        abort(400)
+    except ValueError:
+        # occurs when you try a log(a,0)
+        abort(400)
+
 if __name__ == '__main__':
-    app.run(debug=True)        
-        
+    app.run(debug=True)
