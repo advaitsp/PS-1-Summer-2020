ecf5f6b198fb8d80486fba7b8a19fd566de621d3
advaitsp <65618239+advaitsp@users.noreply.github.com>
2020-06-10 13:37:20 +0530

    Create power.py

---
+++ Numpy/Arithmetic Operations/power.py
@@ -0,0 +1,18 @@
+import numpy as np 
+a = np.array([10,100,1000]) 
+
+print 'Our array is:' 
+print a 
+print '\n'  
+
+print 'Applying power function:' 
+print np.power(a,2) 
+print '\n'  
+
+print 'Second array:' 
+b = np.array([1,2,3]) 
+print b 
+print '\n'  
+
+print 'Applying power function again:' 
+print np.power(a,b)
