4e2058945bcfc1766eb06f3a51f7fcf2f55bff77
advaitsp <65618239+advaitsp@users.noreply.github.com>
2020-06-10 13:37:54 +0530

    Create reciprocal.py

---
+++ Numpy/Arithmetic Operations/reciprocal.py
@@ -0,0 +1,18 @@
+import numpy as np 
+a = np.array([0.25, 1.33, 1, 0, 100]) 
+
+print 'Our array is:' 
+print a 
+print '\n'  
+
+print 'After applying reciprocal function:' 
+print np.reciprocal(a) 
+print '\n'  
+
+b = np.array([100], dtype = int) 
+print 'The second array is:' 
+print b 
+print '\n'  
+
+print 'After applying reciprocal function:' 
+print np.reciprocal(b) 
