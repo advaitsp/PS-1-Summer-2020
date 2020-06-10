23ec245230727f7e823bd7a8d78944a606f43a44
advaitsp <65618239+advaitsp@users.noreply.github.com>
2020-06-10 13:44:42 +0530

    Create rounding_functions.py

---
+++ Numpy/Functions/rounding_functions.py
@@ -0,0 +1,11 @@
+import numpy as np 
+a = np.array([1.0,5.55, 123, 0.567, 25.532]) 
+
+print 'Original array:' 
+print a 
+print '\n'  
+
+print 'After rounding:' 
+print np.around(a) 
+print np.around(a, decimals = 1) 
+print np.around(a, decimals = -1)
