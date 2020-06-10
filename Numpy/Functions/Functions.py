312be46b5092783d888c6ded5c0f12a273e1c3b0
advaitsp <65618239+advaitsp@users.noreply.github.com>
2020-06-10 13:42:48 +0530

    Create Functions.py

---
+++ Numpy/Functions/Functions.py
@@ -0,0 +1,44 @@
+import numpy as np 
+a = np.array([0,30,45,60,90]) 
+
+print 'Array containing sine values:' 
+sin = np.sin(a*np.pi/180) 
+print sin 
+print '\n'  
+
+print 'Compute sine inverse of angles. Returned values are in radians.' 
+inv = np.arcsin(sin) 
+print inv 
+print '\n'  
+
+print 'Check result by converting to degrees:' 
+print np.degrees(inv) 
+print '\n'  
+
+print 'arccos and arctan functions behave similarly:' 
+cos = np.cos(a*np.pi/180) 
+print cos 
+print '\n'  
+
+print 'Inverse of cos:' 
+inv = np.arccos(cos) 
+print inv 
+print '\n'  
+
+print 'In degrees:' 
+print np.degrees(inv) 
+print '\n'  
+
+print 'Tan function:' 
+tan = np.tan(a*np.pi/180) 
+print tan
+print '\n'  
+
+print 'Inverse of tan:' 
+inv = np.arctan(tan) 
+print inv 
+
+print '\n'  
+
+print 'In degrees:' 
+print np.degrees(inv) 
