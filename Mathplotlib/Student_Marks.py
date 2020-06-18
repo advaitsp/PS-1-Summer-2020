f93927c03c6254e296ff775cdfeac6eeeaaee4b6
advaitsp <65618239+advaitsp@users.noreply.github.com>
2020-06-18 14:23:06 +0530

    Create Student_Marks.py

---
+++ Mathplotlib/Student_Marks.py
@@ -0,0 +1,8 @@
+from matplotlib import pyplot as plt 
+x=['Advait','Ridhiman','Rishabh','Sarthak','Shiv/Shankar']
+y=[70,45,75,80,65]
+plt.bar(x,y)
+plt.title("Marks in python")
+plt.xlabel("Student ")
+plt.ylabel("marks")
+plt.show()
